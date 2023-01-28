38480578072c3b5415b29227f4bbb0073e91cf95
thapatechnical <vbthapa55@gmail.com>
2023-01-28 16:27:46 +0530

    reduxtoollkit slices

---
+++ src/store/slices/UserSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const users = createSlice({
+  name: "user",
+  initialState: [],
+  reducers: {
+    addUser(state, action) {},
+    removeUser(state, action) {},
+    deleteUsers(state, action) {},
+  },
+});
+
+export { users };
